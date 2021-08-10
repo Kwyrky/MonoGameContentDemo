ce89f031f829a0f4a3387f8044bad6a074e31546
kwyrky <kwyrky@gmail.com>
2021-08-10 21:41:10 +0200

    added sample project which loads some content

---
+++ MonoGameContentDemo.OpenGL/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameContentDemo.OpenGL
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Effect effect;
+        private Texture2D texture;
+        private SpriteFont font;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            LoadEffectTextureFont();
+        }
+
+        private void LoadEffectTextureFont()
+        {
+            effect = Content.Load<Effect>("effect");
+            texture = Content.Load<Texture2D>("texture");
+            font = Content.Load<SpriteFont>("font");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+            bool isBackButtonPressed = gamePadState.Buttons.Back == ButtonState.Pressed;
+            if (isBackButtonPressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.MonoGameOrange);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGameContentDemo.OpenGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoGameContentDemo.OpenGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
